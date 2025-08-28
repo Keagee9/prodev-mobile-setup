b4149ffdcfa681062cf37592abcd2a408756cde2
Keagee9 <Kingsleyfrancis.kalu@gmail.com>
2025-08-28 19:56:09 +0100

    week 10

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -6,14 +6,14 @@ export default function Index() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <ImageBackground
-          source={require("../assets/images/background-image.png")}
+          source={require("@/assets/images/background-image.png")}
           style={styles.background}
           resizeMode="cover"
         >
           <View style={styles.container}>
             <View>
               <View style={styles.companyLogo}>
-                <Image source={require("../assets/images/Logo.png")} style={styles.logo} />
+                <Image source={require("@/assets/images/background-image.png")} style={styles.logo} />
               </View>
 
               <View style={styles.textGroup}>
